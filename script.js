d4de5ffe80ba474532d71713c871e554f6ad9d21
Alhambra Ferdinando <105513029+NanoKeboku@users.noreply.github.com>
2024-06-17 04:25:53 -0500

    card fix

--- script.js
+++ script.js
@@ -10,114 +10,119 @@ const sampah = {
   kertas: {
     kertasPutih: {
       nama: 'Kertas Putih / HVS',
+      img : 'image/kertas.png',
       deskripsi: 'Kertas putih atau HVS yang sering digunakan untuk mencetak dokumen dan tugas sekolah.'
     },
     koran: {
       nama: 'Koran',
+      img : 'image/koran.jpg',
       deskripsi: 'Kertas bekas koran yang digunakan untuk membaca berita harian.'
     },
     bukuBekas: {
       nama: 'Buku Bekas',
+      img : 'image/buku.jpg',
       deskripsi: 'Buku yang sudah tidak digunakan lagi, bisa berupa buku pelajaran atau novel.'
-    },
-    kertasWarna: {
-      nama: 'Kertas Warna / Duplek',
-      deskripsi: 'Kertas berwarna yang sering digunakan untuk kerajinan atau keperluan dekoratif.'
-    },
-    kertasBuram: {
-      nama: 'Kertas Buram',
-      deskripsi: 'Kertas dengan permukaan tidak mengkilap, biasanya digunakan untuk konsep atau catatan.'
     },
     karton: {
       nama: 'Karton',
+      img : 'image/karton.jpg',
       deskripsi: 'Kertas tebal yang biasanya digunakan untuk kemasan atau pembuatan kotak.'
     }
   },
   plastik: {
     gelasPlastik: {
       nama: 'Gelas Plastik',
+       img : 'image/gelas.jpg',
       deskripsi: 'Gelas sekali pakai yang terbuat dari plastik, sering digunakan untuk minuman.'
     },
     botolPlastik: {
       nama: 'Botol Plastik',
+       img : 'image/botol.jpg',
       deskripsi: 'Botol yang terbuat dari plastik, biasanya digunakan untuk air mineral atau minuman lainnya.'
     },
-    damar: {
-      nama: 'Damar',
-      deskripsi: 'Jenis plastik yang digunakan dalam berbagai produk, termasuk mainan dan peralatan rumah tangga.'
+    perabotPlastik: {
+      nama: 'Perabot Plastik',
+       img : 'image/perabot.jpg',
+      deskripsi: 'Peralatan rumah tangga yang terbuat dari plastik, termasuk kursi, meja, dan wadah penyimpanan.'
     }
   },
   alumunium:{
-    kaleng: {
-      nama: 'Kaleng',
-      deskripsi: 'Wadah alumunium yang digunakan untuk kemasan makanan dan minuman.'
+    perabotanAlumunium: {
+      nama: 'Perabotan Alumunium',
+      img : 'image/caldron.jpg',
+      deskripsi: 'Peralatan rumah tangga yang terbuat dari alumunium, termasuk panci, wajan, dan alat masak lainnya.'
     },
-    foil: {
-      nama: 'Foil',
-      deskripsi: 'Lembaran tipis alumunium yang sering digunakan untuk membungkus makanan.'
-    },
-    tutupBotol: {
-      nama: 'Tutup Botol Alumunium',
-      deskripsi: 'Tutup botol yang terbuat dari alumunium, sering kali dari botol minuman.'
-    },
-    kemasanAlumunium: {
-      nama: 'Kemasan Alumunium',
-      deskripsi: 'Kemasan lain yang terbuat dari alumunium, seperti bekas permen karet atau obat.'
-    }
+  seng: {
+    nama: 'Seng',
+    img : 'image/seng.webp',
+    deskripsi: 'Lembaran logam yang biasanya digunakan untuk atap dan dinding pada bangunan.'
+  },
+  kaleng: {
+    nama: 'Kaleng',
+    img : 'image/kaleng.jpg',
+    deskripsi: 'Wadah yang terbuat dari alumunium, biasanya digunakan untuk minuman atau makanan kaleng.'
+  }
   },
   besiLogam: {
     besi: {
-      nama: 'Besi',
+    img : 'image/besi.jpg',
+    nama: 'Besi',
       deskripsi: 'Material logam yang kuat dan tahan lama, sering digunakan dalam konstruksi dan peralatan.'
     },
     baja: {
       nama: 'Baja',
+    img : 'image/steel.jpg',
       deskripsi: 'Paduan besi yang lebih kuat dan lebih ringan, digunakan dalam berbagai aplikasi industri.'
     },
     logamLain: {
-      nama: 'Logam Lainnya',
+    img : 'image/kuningan.jpg',
+    nama: 'Logam Lainnya',
       deskripsi: 'Material logam lain yang dapat didaur ulang, seperti tembaga, aluminium, dan seng.'
     }
   },
   elektronik: {
     ponsel: {
-      nama: 'Ponsel',
+    img : 'image/hp.jpg',
+    nama: 'Ponsel',
       deskripsi: 'Perangkat komunikasi nirkabel yang digunakan untuk telepon, pesan teks, dan internet.'
     },
-    komputer: {
-      nama: 'Komputer',
-      deskripsi: 'Perangkat elektronik untuk memproses data dan melakukan berbagai tugas komputasi.'
+    mesinCuci: {
+    img : 'image/mc.jpg',
+    nama: 'Mesin Cuci',
+      deskripsi: 'Perangkat elektronik yang digunakan untuk mencuci pakaian secara otomatis.'
+    },
+    ac: {
+      nama: 'AC',
+    img : 'image/ac.jpg',
+      deskripsi: 'Perangkat elektronik yang digunakan untuk mengatur suhu dan kelembaban udara di dalam ruangan.'
     },
     tv: {
-      nama: 'Televisi',
+    img : 'image/tv.jpg',
+    nama: 'Televisi',
       deskripsi: 'Alat untuk menerima sinyal dan menampilkan gambar bergerak dan suara.'
     },
     perangkatElektronikLain: {
-      nama: 'Perangkat Elektronik Lainnya',
+    img : 'image/printer.jpg',
+    nama: 'Perangkat Elektronik Lainnya',
       deskripsi: 'Termasuk perangkat seperti printer, scanner, dan perangkat keras lainnya.'
     }
   },
   botolKaca: {
     botolMinuman: {
       nama: 'Botol Minuman Kaca',
-      deskripsi: 'Botol yang terbuat dari kaca, biasanya digunakan untuk minuman seperti anggur dan bir.'
-    },
-    botolObat: {
-      nama: 'Botol Obat Kaca',
-      deskripsi: 'Botol yang terbuat dari kaca, digunakan untuk kemasan obat-obatan.'
-    },
-    botolLainnya: {
-      nama: 'Botol Kaca Lainnya',
-      deskripsi: 'Botol kaca lainnya yang digunakan untuk berbagai keperluan seperti kosmetik dan perawatan pribadi.'
+       img : 'image/botolKaca.jpg',
+      deskripsi: 'Botol yang terbuat dari kaca, biasanya digunakan untuk minuman seperti kecap dan bir.'
     }
   },
   daun: {
     daunKering: {
-      nama: 'Daun Kering',
+       img : 'image/daunK.jpg',
+       nama: 'Daun Kering',
       deskripsi: 'Daun yang sudah gugur dan mengering dari pohon atau tanaman.'
     },
     daunSegar: {
-      nama: 'Daun Segar',
+       img : 'image/daunB.jpg',
+       nama: 'Daun Segar',
       deskripsi: 'Daun yang masih segar dan baru dipetik dari pohon atau tanaman.'
     }
   }
@@ -135,7 +140,7 @@ function cardGenerator(param) {
           <div class="card-image">
             <img
               style="width: 100%; height: 100%; object-fit: cover"
-              src=""
+              src="${item.img}"
               alt=""
             />
           </div>
@@ -149,9 +154,21 @@ function cardGenerator(param) {
     parentCard.insertAdjacentHTML('beforeend', card);
   });
 }
+const buttons = document.querySelectorAll('.toggle-button');
 
 // Fungsi event handler untuk tombol
+buttons[0].classList.toggle('actived');
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Remove 'actived' class from all buttons
+    buttons.forEach(btn => btn.classList.remove('actived'));
+    button.classList.toggle('actived');
+    
+    // Add 'actived' class to the clicked button
+  });
+});
 function handleButtonClick(category) {
+
   switch (category) {
     case 'kertas':
       cardGenerator(sampah.kertas);
@@ -179,3 +196,4 @@ function handleButtonClick(category) {
   }
 }   
 cardGenerator(sampah.kertas);
+
